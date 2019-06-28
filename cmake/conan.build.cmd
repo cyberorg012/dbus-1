6c11918d383e7ceff3105af7dc14d0b18e9003a7
Tom Nelson <tom.nelson@barco.com>
2019-06-27 20:08:47 -0700

    setting the correct refrence path to conan cmake file

--- cmake/conan.build.cmd
+++ cmake/conan.build.cmd
@@ -1,9 +1,9 @@
 if "%APPVEYOR_REPO_TAG%" == "true" (
-    conan create %APPVEYOR_BUILD_FOLDER% "%APPVEYOR_PROJECT_NAME%/%APPVEYOR_REPO_TAG_NAME:v=%@barco/healthcare"
+    conan create %APPVEYOR_BUILD_FOLDER%\cmake "%APPVEYOR_PROJECT_NAME%/%APPVEYOR_REPO_TAG_NAME:v=%@barco/healthcare"
     conan upload --remote barco --all %APPVEYOR_PROJECT_NAME%/%APPVEYOR_REPO_TAG_NAME:v=%@barco/healthcare
 ) else (
     if "%APPVEYOR_REPO_BRANCH%" == "barco_1.12.16" (
-        conan create %APPVEYOR_BUILD_FOLDER% %APPVEYOR_PROJECT_NAME%/1.12.16.0@barco/healthcare
+        conan create %APPVEYOR_BUILD_FOLDER%\cmake %APPVEYOR_PROJECT_NAME%/1.12.16.0@barco/healthcare
         conan upload --remote barco --all --force %APPVEYOR_PROJECT_NAME%/1.12.16.0@barco/healthcare
     )
 )
