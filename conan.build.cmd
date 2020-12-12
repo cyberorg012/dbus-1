5cd1e1af9f79b69c9597c3386a9cb0cf72971b24
Frederik Vannoote <frederik.vannoote@barco.com>
2020-12-12 22:40:08 +0100

    Bump package version

--- conan.build.cmd
+++ conan.build.cmd
@@ -3,7 +3,7 @@ if "%APPVEYOR_REPO_TAG%" == "true" (
     conan upload --remote barco --all %APPVEYOR_PROJECT_NAME%/%APPVEYOR_REPO_TAG_NAME:v=%@barco/healthcare
 ) else (
     if "%APPVEYOR_REPO_BRANCH%" == "barco_1.12.16" (
-        conan create %APPVEYOR_BUILD_FOLDER%\cmake %APPVEYOR_PROJECT_NAME%/1.12.16.0@barco/healthcare
-        conan upload --remote barco --all --force %APPVEYOR_PROJECT_NAME%/1.12.16.0@barco/healthcare
+        conan create %APPVEYOR_BUILD_FOLDER%\cmake %APPVEYOR_PROJECT_NAME%/1.12.16.1@barco/healthcare
+        conan upload --remote barco --all --force %APPVEYOR_PROJECT_NAME%/1.12.16.1@barco/healthcare
     )
 )
